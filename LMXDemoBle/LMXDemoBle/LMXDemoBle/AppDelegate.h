314b12da959dc1ad0c572a44e5578c89ca3f26e5
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2014-09-21 15:41:55 -0400

    :MXDemoBle example.  Send message to LMX displays via Bluetooth.

---
+++ LMXDemoBle/LMXDemoBle/LMXDemoBle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LMXDemoBle
+//
+//  Created by Hoan on 2014-09-20.
+//  Copyright (c) 2014 I-SYST inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

bc2ad3f6d8dcc60b269041a12d7ec2c8d0fe8325
Hoan Hoang <hnhoan@i-syst.com>
2015-08-02 10:18:33 -0400

    Initial commit

--- UartBle/UartBle/DetailViewController.m
+++ UartBle/UartBle/DetailViewController.m
@@ -1,4 +1,36 @@
-//
+/*--------------------------------------------------------------------------
+ File   : DetailViewController.m
+ 
+ Author : Hoang Nguyen Hoan          Sep. 17, 2014
+ 
+ Desc   : UART over custom BLE exemple
+ 
+ Copyright (c) 2014, I-SYST inc., all rights reserved
+ 
+ Permission to use, copy, modify, and distribute this software for any purpose
+ with or without fee is hereby granted, provided that the above copyright
+ notice and this permission notice appear in all copies, and none of the
+ names : I-SYST or its contributors may be used to endorse or
+ promote products derived from this software without specific prior written
+ permission.
+ 
+ For info or contributing contact : hnhoan at i-syst dot com
+ 
+ THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ ----------------------------------------------------------------------------
+ Modified by          Date              Description
+ 
+ ----------------------------------------------------------------------------*///
 //  DetailViewController.m
 //  UartBle
 //
@@ -7,6 +39,11 @@
 //
 
 #import "DetailViewController.h"
+
+int g_BaudrateTable[] = {
+    1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 76800, 115200, 230400,
+    250000, 460800, 921600
+};
 
 @interface DetailViewController ()
 @property (strong, nonatomic) UIPopoverController *masterPopoverController;
@@ -24,6 +61,14 @@
         
         // Update the view.
         [self configureView];
+        CBPeripheral *peripheral = (CBPeripheral*)newDetailItem;
+        CBUUID *servUUID = [CBUUID UUIDWithString:UART_SERVICE_UUID];
+        
+        peripheral.delegate = self;
+        
+        //    BlueIODev *dev = [[BlueIODev alloc] initWithPeripheral:peripheral andCentral:central];
+        //    if (peripheral == _CurrPeripheral)
+        [peripheral discoverServices:@[servUUID]];
     }
 
     if (self.masterPopoverController != nil) {
@@ -35,9 +80,9 @@
 {
     // Update the user interface for the detail item.
 
-    if (self.detailItem) {
-        self.detailDescriptionLabel.text = [self.detailItem description];
-    }
+    //if (self.detailItem) {
+    //    self.detailDescriptionLabel.text = [self.detailItem description];
+    //}
 }
 
 - (void)viewDidLoad
@@ -51,6 +96,20 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) writeString:(NSString *)string
+{
+    CBPeripheral *peripheral = (CBPeripheral*)_detailItem;
+    NSData *data = [NSData dataWithBytes:string.UTF8String length:string.length];
+    
+    [peripheral writeValue:data forCharacteristic:self.dataCharacteristic type:CBCharacteristicWriteWithoutResponse];
+}
+
+- (IBAction)sendButtonPressed:(id)sender
+{
+    UITextField* txt  = (UITextField*)[self.view viewWithTag:2];
+    [self writeString:txt.text];
 }
 
 #pragma mark - Split view
@@ -67,6 +126,143 @@
     // Called when the view is shown again in the split view, invalidating the button and popover controller.
     [self.navigationItem setLeftBarButtonItem:nil animated:YES];
     self.masterPopoverController = nil;
+}
+
+#pragma mark - Picker view
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    //[_currencyTableView reloadData];
+}
+
+// The number of columns of data
+- (int)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+// The number of rows of data
+- (int)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return sizeof(g_BaudrateTable)/sizeof(int);
+}
+
+// The data to return for the row and component (column) that's being passed in
+- (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return [NSString stringWithFormat:@"%d", g_BaudrateTable[row]];
+}
+
+#pragma mark - UITextField
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    NSLog(@"textFieldDidEndEditing entered %@",textField.text);
+    
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSLog(@"textFieldShouldReturn entered %@",textField.text);
+    [textField resignFirstResponder];
+    return YES;
+}
+
+#pragma mark - Bluetooth
+
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error
+{
+    
+    for (CBService *service in peripheral.services) {
+        NSLog(@"Discovered service %@", service);
+        CBUUID *serviceUUID = [CBUUID UUIDWithString:UART_SERVICE_UUID];
+        CBUUID *datacharUUID = [CBUUID UUIDWithString:UART_DATA_CHAR_UUID];
+        
+        if ([service.UUID isEqual:(serviceUUID)])
+        {
+            [peripheral discoverCharacteristics:nil forService:service];
+            
+        }
+    }
+    //[self.BleCentralManager cancelPeripheralConnection:_ConnectedPeripheral];
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(NSError *)error
+{
+    CBUUID *datacharUUID = [CBUUID UUIDWithString:UART_DATA_CHAR_UUID];
+    CBUUID *ctrlcharUUID = [CBUUID UUIDWithString:UART_CTRL_CHAR_UUID];
+    
+    for (CBCharacteristic *characteristic in service.characteristics)
+    {
+        NSLog(@"DetailView Charact %@", characteristic);
+        if ([characteristic.UUID isEqual:(datacharUUID)])
+        {
+            
+            _dataCharacteristic = characteristic;
+//           [characteristic.value  getBytes:data];
+//            [self updateCounts:data];
+            //[_dataTableView reloadData];
+            if (characteristic.properties & CBCharacteristicPropertyNotify)
+                [peripheral setNotifyValue:YES forCharacteristic:characteristic];
+        
+            if (characteristic.properties & CBCharacteristicPropertyWriteWithoutResponse )
+                NSLog(@"DetailView Charact write ok");
+            
+        }
+        if ([characteristic.UUID isEqual:(ctrlcharUUID)])
+        {
+            _ctrlCharacteristic = characteristic;
+        }
+    }
+    
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+{
+    if (error)
+        return;
+    
+    CBUUID *datacharUUID = [CBUUID UUIDWithString:UART_DATA_CHAR_UUID];
+    CBUUID *ctrlcharUUID = [CBUUID UUIDWithString:UART_CTRL_CHAR_UUID];
+
+    if ([characteristic.UUID isEqual:(datacharUUID)])
+    {
+        NSLog(@"DetailView didUpdateValueForCharacteristic data %@", characteristic);
+        UITextView* tview = (UITextView*)[self.view viewWithTag:1];
+        
+        NSString *string = [NSString stringWithUTF8String:[[characteristic value] bytes]];
+        tview.text = [tview.text stringByAppendingFormat:@"%@", string];
+    }
+    
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateNotificationStateForCharacteristic:(CBCharacteristic *)characteristic
+             error:(NSError *)error {
+    
+    if (error) {
+        NSLog(@"Error changing notification state: %@",
+              [error localizedDescription]);
+    }
+    [peripheral readValueForCharacteristic:characteristic];
+    
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didWriteValueForCharacteristic:(CBCharacteristic *)characteristic
+             error:(NSError *)error
+{
+    
+    if (error) {
+        NSLog(@"Error writing characteristic value: %@",
+              [error localizedDescription]);
+    }
+}
+
+- (void)deviceDisconnected
+{
+    // Device was diconnected
+    _detailItem = nil;
+//    memset(newflag, 0, sizeof(newflag));
+//    memset(DiamCounts, 0, sizeof(DiamCounts));
+    NSLog(@"Device disconnected");
 }
 
 @end
--- UartBle/UartBle/MasterViewController.m
+++ UartBle/UartBle/MasterViewController.m
@@ -1,3 +1,36 @@
+/*--------------------------------------------------------------------------
+ File   : MasterViewController.m
+ 
+ Author : Hoang Nguyen Hoan          Sep. 17, 2014
+ 
+ Desc   : UART over custom BLE exemple
+ 
+ Copyright (c) 2014, I-SYST inc., all rights reserved
+ 
+ Permission to use, copy, modify, and distribute this software for any purpose
+ with or without fee is hereby granted, provided that the above copyright
+ notice and this permission notice appear in all copies, and none of the
+ names : I-SYST or its contributors may be used to endorse or
+ promote products derived from this software without specific prior written
+ permission.
+ 
+ For info or contributing contact : hnhoan at i-syst dot com
+ 
+ THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ ----------------------------------------------------------------------------
+ Modified by          Date              Description
+ 
+ ----------------------------------------------------------------------------*/
 //
 //  MasterViewController.m
 //  UartBle
@@ -30,10 +63,15 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
+    //self.navigationItem.leftBarButtonItem = self.editButtonItem;
 
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
+    self.BleCentralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        [self scanBle];
+    });
+
+    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(refreshBleScan:)];
+    self.navigationItem.leftBarButtonItem = addButton;
     self.detailViewController = (DetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
 }
 
@@ -43,14 +81,14 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)insertNewObject:(id)sender
+- (void)refreshBleScan:(id)sender
 {
-    if (!_objects) {
-        _objects = [[NSMutableArray alloc] init];
+    if (_BleCentralManager.state == CBCentralManagerStatePoweredOn)
+    {
+        [_BleCentralManager stopScan];
+        [self scanBle];
+        
     }
-    [_objects insertObject:[NSDate date] atIndex:0];
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
 }
 
 #pragma mark - Table View
@@ -69,8 +107,8 @@
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
 
-    NSDate *object = _objects[indexPath.row];
-    cell.textLabel.text = [object description];
+    CBPeripheral *object = _objects[indexPath.row];
+    cell.textLabel.text = [object name];
     return cell;
 }
 
@@ -108,19 +146,102 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
-        NSDate *object = _objects[indexPath.row];
-        self.detailViewController.detailItem = object;
-    }
+    //if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
+        CBPeripheral *object = _objects[indexPath.row];
+        
+        [self.BleCentralManager stopScan];
+        if (object.state == CBPeripheralStateDisconnected)
+            [self.BleCentralManager connectPeripheral:object options:nil];
+        
+//        self.detailViewController.detailItem = object;
+   // }
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([[segue identifier] isEqualToString:@"showDetail"]) {
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        NSDate *object = _objects[indexPath.row];
+        CBPeripheral *object = _objects[indexPath.row];
         [[segue destinationViewController] setDetailItem:object];
     }
 }
+
+#pragma mark BLE
+
+- (void)centralManager:(CBCentralManager *)central didRetrieveConnectedPeripherals:(NSArray *)peripherals
+{
+    NSLog(@"Connected peripheral %@", peripherals);
+    
+}
+
+- (void)centralManagerDidUpdateState:(CBCentralManager *)central
+{
+    //    NSLog(@"Central %@", central.state);
+    
+}
+
+- (void) scanBle
+{
+    [_objects removeAllObjects];
+    [self.tableView reloadData];
+    
+    if (_BleCentralManager.state == CBCentralManagerStatePoweredOn)
+    {
+        
+        // Scans for any peripheral
+        CBUUID *serviceUUID = [CBUUID UUIDWithString:UART_SERVICE_UUID];
+        
+        NSArray *peripherals = [_BleCentralManager retrieveConnectedPeripheralsWithServices:@[serviceUUID]];
+        if (peripherals.count <= 0)
+            [self.BleCentralManager scanForPeripheralsWithServices:@[serviceUUID] options:nil];
+        else
+        {
+            NSLog(@"Found Connected peripheral %@", peripherals);
+            //            [_DeviceList addObject:peripherals[0]];//[NSString stringWithFormat:@"%@", peripheral.name]];
+            //            [_DevicePickerView reloadAllComponents];
+            for (CBPeripheral *peripheral in peripherals)
+            {
+                //                _CurrPeripheral = peripheral;
+                //peripheral.delegate = self;
+                // [self.BleCentralManager connectPeripheral:peripheral options:nil];
+            }
+        }
+    }
+}
+
+- (void) deviceFound: (CBPeripheral*)peripheral
+{
+    NSLog(@"deviceFound peripheral %@", peripheral);
+    
+}
+
+- (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI {
+    
+    if (!_objects)
+    {
+        _objects = [[NSMutableArray alloc] init];
+    }
+    [_objects insertObject:peripheral atIndex:0];
+    [self.tableView reloadData];
+}
+
+- (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral
+{
+    CBUUID *servUUID = [CBUUID UUIDWithString:UART_SERVICE_UUID];
+    
+    //    BlueIODev *dev = [[BlueIODev alloc] initWithPeripheral:peripheral andCentral:central];
+    //    if (peripheral == _CurrPeripheral)
+    self.detailViewController.detailItem = peripheral;
+    NSLog(@"Peripheral connected");
+    //[peripheral discoverServices:nil];//@[servUUID]];
+}
+
+- (void)centralManager:(CBCentralManager *)central didDisconnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error
+{
+    
+    //    [self.detailViewController deviceDisconnected];
+    [self scanBle];
+}
+
 
 @end
